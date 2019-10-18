740dc9c8c1ccfadcff617cf46d803f4281e7580c
lewypiotrek <lewypiotrek.lewandowski@.github.com>
2019-10-18 15:30:19 +0200

    Algorithms

--- QueuePL.h
+++ QueuePL.h
@@ -1,35 +1,111 @@
 #pragma once
 /*
-	Piotr Lewandowski - Kolejka na int
+	Piotr Lewandowski - Kolejka 
+	15.10.2019r
 */
 #include <iostream>
 
-
+template <class Te>
 struct ElementQueue
 {
 	//int * pre;
-	int value;
+	Te value;
 	ElementQueue * next;
-	ElementQueue(int v)
+	ElementQueue(Te v)
 	{
 		this->value = v;
 		this->next = nullptr;
 	}
 };
 
+template <class Tc>
 class QueuePL
 {
 public:
-	ElementQueue * first;
-	ElementQueue * last;
+	ElementQueue<Tc> * first;
+	ElementQueue<Tc> * last;
 
 	QueuePL();
 	~QueuePL();
 
-	void add(int v);
+	void add(Tc v);
 	void view();
 	int size();
 	void remove();
 
 };
 
+
+///////////////////////////////////////////////////////
+
+
+template <class Tc>
+QueuePL<Tc>::QueuePL()
+{
+	first = nullptr;
+	last = nullptr;
+}
+
+template <class Tc>
+QueuePL<Tc>::~QueuePL()
+{
+	delete[]first;
+	delete[]last;
+}
+
+template <class Tc>
+void QueuePL<Tc>::add(Tc v)
+{
+	ElementQueue<Tc> * e = new ElementQueue<Tc>(v);
+
+	// sprawdzam czy jest to pierwszy ElementQueue w kolejce
+	if (first == nullptr || last == nullptr)
+	{
+		first = e;
+		last = e;
+	}
+	else
+	{
+		last->next = e; // ostatni ElementQueue teraz wskazuje na obecnie dodawany
+		last = e;	    // obecnie dodawany staje siê ostatnim ElementQueueem
+	}
+}
+
+template <class Tc>
+void QueuePL<Tc>::view()
+{
+	ElementQueue<Tc> * temp = first;
+
+	while (temp != nullptr)
+	{
+		std::cout << temp->value << " ";
+		temp = temp->next;
+	}
+}
+
+template <class Tc>
+int QueuePL<Tc>::size()
+{
+	ElementQueue<Tc> * temp = first;
+	int size = 0;
+
+	while (temp != nullptr)
+	{
+		size++;
+		temp = temp->next;
+	}
+
+	return size;
+
+}
+
+template <class Tc>
+void QueuePL<Tc>::remove()
+{
+	if (first != nullptr)
+	{
+		ElementQueue<Tc> * temp = first;
+		first = temp->next;
+		delete temp;
+	}
+}
